338ae7b5b23f5b0365b5fa61778ac816b874d60f
garikmirakyan06 <garikmirakyan06@gmail.com>
2025-04-22 14:39:15 -0700

    some changes

--- build/test/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/garik/Desktop/SearchTreeClone1/Binary-Search-Tree-implementation/test/external/googletest/googletest
-# Build directory: /home/garik/Desktop/SearchTreeClone1/Binary-Search-Tree-implementation/build/test/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
